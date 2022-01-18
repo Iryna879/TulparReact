eb040fea6980253f9ec79611497195052e39089a
Iryna <irysukd111@gmail.com>
2022-01-18 13:19:53 +0200

    mediaWhatIsBlock finish

--- front/src/components/otherComponents/whatIsBlock.js
+++ front/src/components/otherComponents/whatIsBlock.js
@@ -25,7 +25,7 @@ const  WhatIsBlock = (props) => {
                 <div className="container-fluid p-0 centerInfo" style={{'backgroundImage': `url(${"http://localhost:3001" + item.background})`}}>
                     <div className="container">
                         <div className="row  ">
-                            <div className="col-xl-6 offset-xl-3 col-sm-8">
+                            <div className="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-sm-6 offset-sm-3">
                                 <div className="textAboutCenter">
                                     <h6>{item.header}</h6>
                                     <p>{item.paragraphOne}</p>
