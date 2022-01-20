f84b83ac256bd74c13da4ad46e07ec27123a51da
Iryna <irysukd111@gmail.com>
2022-01-20 13:25:24 +0200

    infoSpecialists

--- front/src/components/otherComponents/infoSpecialists.js
+++ front/src/components/otherComponents/infoSpecialists.js
@@ -1,17 +1,54 @@
-import React, {useEffect, useState} from "react";
-import "./../../style/specialists.css";
+import React from "react";
+/*import "./../../style/specialists.css";*/
 
 
 export function  InfoSpecialists (props)  {
   const specialist = props.specialist;
   const emp = props.selected;
-  let fullName = "";
+  return(  specialist.map(s =>  {
+        if (s._id === emp) {
+            return (
+                <div className="specialistInfo">
+
+                    <p>{s.name + " " + s.surname}</p>
+                    <ul className="gridSpecialistInfo">
+                        <li>Прізвище/Iм'я:</li>
+                        <li>{s.name + " " + s.surname}</li>
+                        <li>Посада:</li>
+                        <li>{s.jobTitle}</li>
+                        <li>Спеціалізація:</li>
+                        <li>{s.specialization}</li>
+                        <li>Кар'єра:</li>
+                        <li>
+                            <ul className="career">
+                                {s.career.split(",").map(cm =>
+                                    <li>{cm}</li>
+                                )}
+                            </ul>
+                        </li>
+                        <li>Наукова діяльність:</li>
+                        <li>
+                            <ul className="activity">
+                                {s.activity.split(",").map(am =>
+                                    <li>{am}</li>
+                                )}
+                            </ul>
+                        </li>
+                    </ul>
+                </div> )
+                }
+                }
+                )
+                )
+                }
+
+{/* let fullName = "";
   let job = "";
   let department = "";
   let career = "";
       let activity = "";
   let careerMass = []; let activityMass = [];
-  specialist.map(s => {
+  specialist.map(s =>  {
       if (s._id === emp) {
           fullName = s.name + " " + s.surname;
           job = s.jobTitle;
@@ -53,6 +90,7 @@ export function  InfoSpecialists (props)  {
             </li>
             </ul>
         </div>
-    )
+    )*/}
 
-}
+
+
