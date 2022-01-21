c81e395af634e17d1c470314bd5bc1406b225568
Iryna <irysukd111@gmail.com>
2022-01-21 14:47:47 +0200

    media 768&480

--- front/src/components/otherComponents/indexHeader.js
+++ front/src/components/otherComponents/indexHeader.js
@@ -34,15 +34,14 @@ export function IndexHeader (props){
                     <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3 logo">
                         <Link to="/"><img src={logo} alt="logo"/></Link>
                     </div>
-                    <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-1 col-md-8 offset-md-1
-                    col-sm-8 offset-sm-1 col-8 offset-1 ">
+                    <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-1 col-md-6 offset-md-3
+                    col-sm-6 offset-sm-3 col-9 ">
                         <ul className="menu">
                             <li><Link to="/">Головна</Link></li>
                             <li><Link to="/services">Послуги</Link></li>
                             <li><Link to="/ourSpecialists">Спеціалісти</Link></li>
                             <li><Link to="/contact">Ми</Link></li>
                             <li><Link to="/news">Новини</Link></li>
-                            <li>УКР</li>
                         </ul>
                     </div>
                 </div>
