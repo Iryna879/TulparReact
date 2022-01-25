87b37983347a1879cdbc78c2aaa718dfdd6718cc
Iryna <irysukd111@gmail.com>
2022-01-25 18:59:55 +0200

    footer 576

--- front/src/components/otherComponents/footer.js
+++ front/src/components/otherComponents/footer.js
@@ -10,10 +10,10 @@ export function Footer() {
         <footer className="container-fluid p-0">
             <div className="container">
                 <div className="row  ">
-                    <div className="col-xl-3 col-lg-4 col-md-4 col-sm-4 col-4 logoFooter">
+                    <div className="col-xl-3 col-lg-4 col-md-4 col-sm-4 col-12 logoFooter">
                         <Link to="/"><img src={logo} alt="alt"/></Link>
                     </div>
-                    <div className="col-xl-2 col-lg-2 col-md-2 col-sm-3 col-3  mapSite">
+                    <div className="col-xl-2 col-lg-2 col-md-2 col-sm-3 col-4  mapSite">
                         <h2>Мапа сайту</h2>
                         <ul>
                             <li><Link to="/">Головна</Link></li>
@@ -25,7 +25,7 @@ export function Footer() {
                     </div>
 
                     <div className="col-xl-2 offset-xl-1 col-lg-2 offset-lg-2
-                    col-md-2 offset-md-2 cols-sm-3 col-3 contactUs">
+                    col-md-2 offset-md-2 cols-sm-3 col-6 contactUs">
                         <h2>Зв`язатися з нами</h2>
                         <ul>
                             <li>+38 093 032 12 19</li>
