7a13b502df166dd94f12b88eb808928e4445ff4d
Iryna <irysukd111@gmail.com>
2022-01-26 15:13:20 +0200

    media576

--- front/src/components/otherComponents/whyWe.js
+++ front/src/components/otherComponents/whyWe.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./../../style/whyWe.css";
-import ellipse from "./../../img/EllipseWhyWe.png";
+import mind from "@img/mind.png";
+import bed from "./../../img/bed.png";
+import smile from "./../../img/smile.png";
+import like from "./../../img/like.png";
 import "./../../media/mediaWhyWe.css";
 
 export function WhyWe () {
@@ -12,16 +15,16 @@ export function WhyWe () {
                     <div className="col-12 whyWe">
                         <h6>Чому ми?</h6>
                         <ul className="benefits">
-                            <li><img src={ellipse} alt="ellipse"/><h1>Перевага 1</h1>
+                            <li><img src={mind} alt="mind"/><h1>Перевага 1</h1>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                     Lorem Ipsum has been the industry's standard dummy text</p></li>
-                            <li><img src={ellipse} alt="ellipse"/><h1>Перевага 2</h1>
+                            <li><img src={bed} alt="bed"/><h1>Перевага 2</h1>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                     Lorem Ipsum has been the industry's standard dummy text</p></li>
-                            <li><img src={ellipse} alt="ellipse"/><h1>Перевага 3</h1>
+                            <li><img src={smile} alt="smile"/><h1>Перевага 3</h1>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                     Lorem Ipsum has been the industry's standard dummy text</p></li>
-                            <li><img src={ellipse} alt="ellipse"/><h1>Перевага 4</h1>
+                            <li><img src={like} alt="like"/><h1>Перевага 4</h1>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                     Lorem Ipsum has been the industry's standard dummy text</p></li>
                         </ul>
