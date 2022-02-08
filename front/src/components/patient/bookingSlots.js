9851888906d6f94134763a5a65aed55d26f59840
Iryna <irysukd111@gmail.com>
2022-02-08 19:56:01 +0200

    bookingSlots media

--- front/src/components/patient/bookingSlots.js
+++ front/src/components/patient/bookingSlots.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import ProfileMenu from "./profileMenu";
+import "./style/bookingSlots.css";
 
 const BookingSlots = () => {
     const location = useLocation();
