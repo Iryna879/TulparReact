6d97075dcd10d99e280d6805cd7b1eaa414fc8ff
Iryna <irysukd111@gmail.com>
2022-02-09 19:46:24 +0200

    menu

---
+++ front/src/components/otherComponents/menu.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+const Menu = (props) => {
+  const name = props.name;
+    return (
+        <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-1 col-md-6 offset-md-3
+                    col-sm-6 offset-sm-3 col-3 offset-3 ">
+            <input id="menu__toggle" type="checkbox"/>
+            <label className="menu__btn" htmlFor="menu__toggle">
+                <span></span>
+            </label>
+            <ul className={`${name}_menu`}>
+                <li><Link to="/">Головна</Link></li>
+                <li><Link to="/services">Послуги</Link></li>
+                <li><Link to="/ourSpecialists">Спеціалісти</Link></li>
+                <li><Link to="/contact">Ми</Link></li>
+            </ul>
+        </div>
+    )
+};
+
+export default Menu;
--- front/src/components/pages/header.js
+++ front/src/components/pages/header.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
+import Menu from "../otherComponents/menu";
 
 const  Header = (props) => {
     const {name, logo} = props;
@@ -21,23 +22,11 @@ const  Header = (props) => {
     return (
         <header className={`container-fluid p-0 ${name}_fluid`}>
             <div className="container">
-                <div className="row  ">
-                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-6 logo">
+                <div className="row logo align-items-center  ">
+                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-6 ">
                         <Link to="/"><img src={logo} alt="logo" /></Link>
                     </div>
-                    <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-1 col-md-6 offset-md-3
-                    col-sm-6 offset-sm-3 col-2 offset-4 ">
-                        <input id="menu__toggle" type="checkbox"/>
-                        <label className="menu__btn" htmlFor="menu__toggle">
-                            <span></span>
-                        </label>
-                        <ul className={`${name}_menu`}>
-                            <li><Link to="/">Головна</Link></li>
-                            <li><Link to="/services">Послуги</Link></li>
-                            <li><Link to="/ourSpecialists">Спеціалісти</Link></li>
-                            <li><Link to="/contact">Ми</Link></li>
-                        </ul>
-                    </div>
+                    <Menu name={name} />
                 </div>
                 {
                     items
