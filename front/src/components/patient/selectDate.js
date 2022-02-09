733a99e36668d90e09e48752223f02ade55f7373
Iryna <irysukd111@gmail.com>
2022-02-09 14:23:07 +0200

    selectDate mobileMedia

--- front/src/components/patient/selectDate.js
+++ front/src/components/patient/selectDate.js
@@ -20,7 +20,7 @@ const SelectDate = () => {
             <ProfileMenu/>
         <div className="bg-dark container-fluid calendarFluid">
             <div>
-                <div className="row m-5 pt-5 calendar" >
+                <div className="row m-5 mb-1 pt-5 calendar" >
                         <h3 >Оберіть дату</h3>
                         <div className="col-12 d-flex justify-content-center  ">
                             <div>
@@ -45,13 +45,13 @@ const SelectDate = () => {
 
                 <div className="row d-flex justify-content-center ">
                             <div className="col-xl-1 offset-xl-5 col-lg-1 offset-lg-5
-                            col-md-2 offset-md-4 col-sm-2 offset-sm-4 col-5 offset-1 ">
+                            col-md-2 offset-md-4 col-sm-2 offset-sm-4 col-3 offset-3 ">
                                 <Link to="/profile/searchdoctor">
                                     <Button color="danger">Назад</Button>
                                 </Link>
                             </div>
                             <div className="col-xl-6 offset-xl-0 col-lg-5 offset-lg-1
-                            col-md-5 offset-md-1 col-sm-5 offset-sm-1 col-2 offset-4">
+                            col-md-5 offset-md-1 col-sm-5 offset-sm-1 col-5 offset-1">
                                 <Link to="/profile/book-slot"
                                         state={{
                                     date: date,
