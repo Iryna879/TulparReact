9337f03258e4bec5344093e43806a1dd506f5989
Iryna <irysukd111@gmail.com>
2022-02-10 13:46:51 +0200

    header

--- front/src/components/otherComponents/menu.js
+++ front/src/components/otherComponents/menu.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import AuthButton from "../../auth/authButton";
 
 const Menu = (props) => {
   const name = props.name;
     return (
-        <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-1 col-md-6 offset-md-3
-                    col-sm-6 offset-sm-3 col-3 offset-3 ">
+        <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-1 col-md-8 offset-md-1
+                    col-sm-8 offset-sm-1 col-3 offset-3 ">
             <input id="menu__toggle" type="checkbox"/>
             <label className="menu__btn" htmlFor="menu__toggle">
                 <span></span>
@@ -15,6 +16,7 @@ const Menu = (props) => {
                 <li><Link to="/services">Послуги</Link></li>
                 <li><Link to="/ourSpecialists">Спеціалісти</Link></li>
                 <li><Link to="/contact">Ми</Link></li>
+                <li><AuthButton/></li>
             </ul>
         </div>
     )
