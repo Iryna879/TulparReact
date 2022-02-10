bd42a8310fac926448d2894b8deb1002dfc68c85
Iryna <irysukd111@gmail.com>
2022-02-10 21:05:41 +0200

    iframe

--- front/src/components/otherComponents/map.js
+++ front/src/components/otherComponents/map.js
@@ -8,7 +8,7 @@ export function Map () {
         <div className="row ">
             <div className="col-12 map">
                 <h6>Контакти</h6>
-                <iframe
+                <iframe title="Tulpar"
                     src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10888.833651309846!2d31.966696592038534!3d46.97724095674197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1suk!2sua!4v1637686787031!5m2!1suk!2sua">
                 </iframe>
             </div>
